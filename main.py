33dbcd28b13ca77f02ef4396496a5a748fef54bf
qmclouca <qmclouca@gmail.com>
2021-05-15 17:49:54 -0300

    Supports and Loads

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import FlexRM
 
 try:
     # FlexRM.example_of_beam_creation()
-    FlexRM.input_beam_data()
-    FlexRM.beam_creation(FlexRM.lenght_of_beam, FlexRM.start_point)
+    FlexRM.example_of_definition_of_beam_properties()
+    FlexRM.example_of_adding_supports()
+    FlexRM.example_of_adding_loads()
+
+    # FlexRM.input_beam_data()
+    # FlexRM.beam_creation(FlexRM.lenght_of_beam, FlexRM.start_point)
 
 except NameError as e:
     print("There is a problem/Ocorreu um problema: ", e)
--- venv/FlexRM.py
+++ venv/FlexRM.py
@@ -2,6 +2,7 @@ from symbeam import *
 # L = lenght, E = Young Modulus, I = Inertia Modulus
 from sympy.abc import L, E, I
 import sympy
+import matplotlib
 
 
 def input_beam_data():
@@ -70,10 +71,80 @@ def example_of_definition_of_beam_properties():
 
     new_beam.set_inertia(x_start, x_end, value)
     """
+    global new_beam 
     new_beam = beam(L)
 
     new_beam.set_young(0, L / 2, E)  # set Young Modulus on first segment
+    print("Módulo de Young do primeiro segmento foi definido corretamente")
     new_beam.set_young(L / 2, L, E / 10)  # set Young Modulus on second segment
+    print("Módulo de Young do segundo segmento foi definido corretamente")
 
     new_beam.set_inertia(0, L / 2, I)  # set Inertia Moment on first segment
+    print("Momento de inércia do primeiro segmento foi definido corretamente")
     new_beam.set_inertia(L / 2, L, I / 2)  # set Inertia Moment on second segment
+    print("Momento de inércia do segundo segmento foi definido corretamente")
+
+def example_of_adding_supports():
+    """
+    The beam must be connected to the exterior via a given number of supports,
+    which materialise the geometric boundary conditions of the problem.
+    Currently, SymBeam can only solve statically determinate beams, therefore,
+    redundant supports cannot be handled. Supports can be added to the beam by
+    specifying the coordinate and the type of support. Exemplarily, this is
+    accomplished by calling the method add_support()
+
+    The types of support available in SymBeam are:
+    roller : a roller, fixed in the transverse direction and allows rotations in the bending plane
+    pin : a pinned support, fixed in the axial and transverse directions and allows rotations in the bending plane
+    fixed : a fixed/clamped support, all degrees of freedom are constrained (no displacements and no rotation)
+    hinge : allows distinct rotations on the left and right of the point, but does not fix the beam in any direction
+
+    new_beam.add_support(x_coord, type)
+    """
+    new_beam.add_support(0, 'fixed')
+    new_beam.add_support(L, 'roller')
+    new_beam.add_support(3*L/4, 'hinge')
+
+def example_of_adding_loads():
+    """
+    The applied external loads are the missing item for completely defining
+    the beam bending problem. These can be point-type, namely, transverse point
+    loads/forces and moments, and segment-type loads, this is, transverse forces
+    distributed along the span of the beam. Point loads and moments are incorporated
+    by calling the add_point_load() and add_point_moment() methods, which receive
+    the coordinate of the point and the value of the load. Distributed loads are
+    applied by calling the add_distributed_load() method, which takes the starting
+    and ending point of the distributed load and the associated expression.
+    """
+    new_beam.add_point_load(3 * L / 4, -P)
+    new_beam.add_point_moment(L, M)
+    new_beam.add_distributed_load(0, L / 2, -q * x)
+
+def example_of_solving():
+    """
+    After specifying the beam properties, supports and loads, the problem
+    can be solved by calling the method solve(). The program will proceed as follows
+    check if the input data is consistent define the individual beam segments,
+    such that each one is associated with a continuous function of the Young modulus,
+    second moment of area and distributed load: in sum, this subdivision must guarantee
+    that the shear force and bending moment diagrams are continuous in each segment
+    and piecewise continuous along the span of the beam solve for the reaction forces
+    and moments of the supports (equilibrium equations) solve for the internal loads
+    (integrate the differential equations for beam equilibrium) solve for the deflections
+    (integrate the elastic curve equation) output the results (can be suppressed if
+    the optional argument output=False): identified segments, exterior reactions,
+    shear force, bending moment, slope and deflection for each beam segment. For the
+    current example, the output shall be as follows. The results can be plotted with
+    matplotlib by calling the method plot on the beam object. The produced figure contains
+    a schematic representation of the problem the shear force diagram the bending moment
+    diagram the deformed shape of the beam. At this stage, to be able to plot the
+    expressions, all the parameters of the problem must be substituted by numerical
+    values, with the natural exception of the x variable, since this is the independent
+    variable. This is can be accomplished by passing the optional argument subs to
+    the plot method. This must be a dictionary whose keys are the string representations
+    of the variables and the values are the effective numerical values. Example
+    substitutions L=2, P=1000, q=5000 and M=1000.
+    """
+    new_beam.solve()
+    new_beam.plot(subs={'P': 1000, 'q': 5000, 'L': 2, 'M': 1000})
+    plt.savefig("beam.pdf")
